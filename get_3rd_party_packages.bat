dd09530cc802b0d9d30c0b4b1d131a93b643351c
Sapporo Ningyo <103877981+NewSapporoNingyo@users.noreply.github.com>
2026-05-28 16:09:07 +0900

    Update get_3rd_party_packages.bat

--- get_3rd_party_packages.bat
+++ get_3rd_party_packages.bat
@@ -1,3 +1,7 @@
-cd third_party
+if not exist "%~dp0third_party\" (
+    mkdir "%~dp0third_party"
+)
+
+cd /d "%~dp0third_party"
 git clone --branch docking https://github.com/ocornut/imgui.git
 git clone https://github.com/epezent/implot.git
